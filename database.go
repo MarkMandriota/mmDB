51f76a70a5602c45346dee93e42647b4bf211784
Nameless Name <62650188+WIDzer557@users.noreply.github.com>
2020-11-21 18:14:18 +0100

    Update database.go

--- database.go
+++ database.go
@@ -7,21 +7,14 @@ import (
 	"syscall"
 )
 
-type (
-	Requester interface {
-		Load(name string) error
-		Unload(name string) error
-	}
-	
-	Req struct {
-		Data map[string]string
-	}
-)	
+type Requester struct {
+	Data map[string]string
+}
 
-func (r *Req) Load(name string) error {
-	file, err := os.OpenFile(name, os.O_CREATE | os.O_RDONLY | syscall.O_CLOEXEC, 0777)
+func (r *Requester) Load(name string) {
+	file, err := os.OpenFile(name, os.O_CREATE|os.O_RDONLY|syscall.O_CLOEXEC, 0777)
 	if err != nil {
-		return err
+		panic(err)
 	}
 
 	reader := bufio.NewReader(file)
@@ -34,14 +27,12 @@ func (r *Req) Load(name string) error {
 
 		r.Data[string(k[:len(k)-1])] = string(v[:len(v)-1])
 	}
-
-	return nil
 }
 
-func (r *Req) Unload(name string) error {
+func (r *Requester) Unload(name string) {
 	file, err := syscall.Open(name, os.O_CREATE | os.O_TRUNC | syscall.O_WRONLY | syscall.O_CLOEXEC, 0777)
 	if err != nil {
-		return err
+		panic(err)
 	}
 
 	buff := make([]byte, 0, 1024)
@@ -49,6 +40,4 @@ func (r *Req) Unload(name string) error {
 		buff = append(buff, []byte(k + "\000" + v + "\000")...)
 	}
 	syscall.Write(file, buff)
-
-	return nil
 }
