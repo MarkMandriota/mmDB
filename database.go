c7c411e815f09880c8ef649098dde5513d61f153
Nameless Name <62650188+WIDzer557@users.noreply.github.com>
2020-11-22 18:26:48 +0100

    Update database.go

--- database.go
+++ database.go
@@ -20,26 +20,28 @@ func (r *Requester) Load(pass string) {
 	defer file.Close()
 
 	reader := bufio.NewReader(file)
+
 	for {
 		k, _ := reader.ReadBytes(0)
 		v, err := reader.ReadBytes(0)
 		if err == io.EOF {
 			break
 		}
+
 		r.Data[(*(*string)(unsafe.Pointer(&k)))[:len(k)-1]] = (*(*string)(unsafe.Pointer(&v)))[:len(v)-1]
 	}
 }
 
 func (r *Requester) Unload(pass string) {
-	file, err := os.OpenFile(pass, O_CREAT|O_TRUNC|O_WRONLY|O_CLOEXEC, 0777)
+	file, err := Open(pass, O_CREAT|O_TRUNC|O_WRONLY|O_CLOEXEC, 0777)
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
+	defer Close(file)
 
-	writer := bufio.NewWriter(file)
+	buff := make([]byte, 0, 4096)
 	for k, v := range r.Data {
-		writer.WriteString(k + "\000" + v + "\000")
+		buff = append(buff, k + "\000" + v + "\000"...)
 	}
-	writer.Flush()
+	Write(file, buff)
 }
